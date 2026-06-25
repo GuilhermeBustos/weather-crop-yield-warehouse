3ed790a53f091fd3458f40f7d792856541688426
Guilherme Bustos <guilherme.bustos@outlook.com>
2026-06-25 14:25:25 -0300

    feat(dbt): add dim_commodity static mart

---
+++ dbt/models/marts/dim_commodity.sql
@@ -0,0 +1,9 @@
+SELECT
+    commodity,
+    display_name,
+    gdd_base_c,
+    bushel_weight_lb
+FROM UNNEST([
+    STRUCT('corn'     AS commodity, 'Corn'     AS display_name, 10 AS gdd_base_c, 56 AS bushel_weight_lb),
+    STRUCT('soybeans' AS commodity, 'Soybeans' AS display_name, 10 AS gdd_base_c, 60 AS bushel_weight_lb)
+])
